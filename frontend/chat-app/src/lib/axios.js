b08aab155a2b14242fdd402cbc61003279be7799
Palakwadhwa <palakwadhwa22@gmail.com>
2026-05-21 22:57:34 +0530

    Fixed axios credentials

--- frontend/chat-app/src/lib/axios.js
+++ frontend/chat-app/src/lib/axios.js
@@ -1,8 +1,6 @@
-import axios  from "axios"
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : 'http://localhost:5000/api',
-    withCredentials:true
-})
-
-export default axiosInstance
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
